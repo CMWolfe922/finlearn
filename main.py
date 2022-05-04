b61073ab10abcfbf28c07910356534111a3c42eb
cmwolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-05-04 09:33:49 -0500

    Fixed main page so it can run on its own

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ if __name__ == '__main__':
     s = time.time()
 
     # =========================================================================== #
+    # STEP 1: GET THE QUOTE AND FUNDAMENTAL DATA AND INSERT TO DATABASE
+    # =========================================================================== #
     # Get the quote and fundamental data using stock_chunks
     quote_data = pd.concat([quote.data(each) for each in stock_chunks])
     fundamental_data = pd.concat([fundamental.data(each)
@@ -30,6 +32,9 @@ if __name__ == '__main__':
     insert_quote_and_fundamental_data_mysql(
         quote_data, fundamental_data, engine)
 
+    # =========================================================================== #
+    # STEP 2: GET THE PRICEHISTORY DATA AND INSERT INTO DATABASE
+    # =========================================================================== #
     # Generator function that will generate one symbol at a time for price
     # history data. This way the function will run once for each symbol
     # stock = generate_symbols()
